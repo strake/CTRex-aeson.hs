6a27896c3da241982c1cb5ae0fee36757bca23ad
M Farkas-Dyck <strake888@gmail.com>
2017-09-05 11:33:16 -0800

    use `labels` defined in CTRex

--- Data/OpenRecords/Aeson.hs
+++ Data/OpenRecords/Aeson.hs
@@ -16,20 +16,15 @@ import Data.OpenRecords
 import Data.Proxy
 import Data.Text (Text)
 import qualified Data.Text as Text (pack)
+import Unconstrained
 
 instance Forall r ToJSON => ToJSON (Rec r) where
     toJSON = Object . eraseToHashMap (Proxy @ToJSON) toJSON
 
-instance Forall r FromJSON => FromJSON (Rec r) where
+instance (Forall r Unconstrained1, Forall r FromJSON) => FromJSON (Rec r) where
     parseJSON (Object o) = rinitAWithLabel (Proxy @FromJSON) $ \ l -> o .: (show' l)
     parseJSON v = typeMismatch msg v
-      where
-        msg = "{" ++ intercalate "," fields ++ "}"
-        fields = labels @r @FromJSON $ rinit (Proxy @FromJSON) undefined
+      where msg = "{" ++ intercalate "," (labels (Proxy @r)) ++ "}"
 
 show' :: Show a => a -> Text
 show' = Text.pack . show
-
--- DUPLICATED because @Data.OpenRecords@ does not export it.
-labels :: forall r c. Forall r c => Rec r -> [String]
-labels = fmap fst . eraseWithLabels (Proxy @c) (\ _ -> undefined)
